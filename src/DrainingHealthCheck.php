9b6a393d5c2d116f04a070a40734338a1e0b05b2
Rob Pungello <rob@rpun.io>
2026-02-17 08:55:00 -0500

    Condense

--- src/DrainingHealthCheck.php
+++ src/DrainingHealthCheck.php
@@ -13,8 +13,7 @@ class DrainingHealthCheck extends Check
     public function run(): Result
     {
         if ($this->isDraining()) {
-            return Result::make()
-                ->failed('The application is currently draining.');
+            return Result::make()->failed('The application is currently draining.');
         } else {
             return Result::make()->ok();
         }
